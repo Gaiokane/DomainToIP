e11e4b54041b2f6eb8d15f4ea1e4b46a91406514
Gaiokane <qk632773809@vip.qq.com>
2020-02-23 15:03:12 +0800

    完成

--- DomainToIP/Form1.cs
+++ DomainToIP/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 
 namespace DomainToIP
 {
@@ -15,6 +16,42 @@ namespace DomainToIP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = GetIP(textBox1.Text.Trim());
+        }
+
+        private string GetIP(string hostname)
+        {
+            try
+            {
+                hostname = hostname.Replace("http://", "").Replace("https://", "").Replace("/", "");
+                string result = string.Empty;
+                IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+                return result = addresses[0].ToString();
+                foreach (IPAddress address in addresses)
+                {
+                    result += address + "\n";
+                }
+                return result;
+
+                /*IPHostEntry hostEntry = Dns.GetHostEntry(hostname);
+                IPEndPoint ipEndPoint = new IPEndPoint(hostEntry.AddressList[0], 0);
+                return ipEndPoint.Address.ToString();*/
+
+                /*IPHostEntry hostEntry = Dns.GetHostEntry(hostname);
+                foreach (IPAddress item in hostEntry.AddressList)
+                {
+                    result += item + "\n";
+                }
+                return result;*/
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
